10b9f0c26966b8ff0f93f948772f716cc2e6c777
KK-tech-byte1991 <84914086+KK-tech-byte1991@users.noreply.github.com>
2023-09-02 14:11:53 +0530

    1 binaries uploaded

---
+++ src/Carousal/Carousal.jsx
@@ -0,0 +1,33 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import React from 'react';
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
+
+const ThemeCarousal = () => {
+  return (
+    <Carousel responsive={responsive}>
+      <div>Item 1</div>
+      <div>Item 2</div>
+      <div>Item 3</div>
+      <div>Item 4</div>
+    </Carousel>
+  );
+};
+
+export default ThemeCarousal;
