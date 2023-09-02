7e023dd99982b4c60e3b63114804f330e532d280
KK-tech-byte1991 <84914086+KK-tech-byte1991@users.noreply.github.com>
2023-09-02 17:37:33 +0530

    3 files modified, 1 added

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,14 +1,29 @@
 import { Grid } from '@mui/material';
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import Carousal from '../Carousal/Carousal';
 import ImageCard from '../ImageCard/ImageCard';
+import locationData from '../server/Location';
+
 const HomePage = () => {
+  const [data, setData] = useState(locationData);
+  useEffect(() => {}, []);
+
+  let arr = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+
   return (
-    <Grid>
+    <Grid className="mx-auto">
       <Header />
       <Carousal />
-      <ImageCard/>
+      <Grid container spacing={3} className="ms-5 me-2 mt-2">
+        {locationData.results.map((ar) => (
+          <Grid item>
+            <ImageCard locDetails={ar} />
+          </Grid>
+        ))}
+      </Grid>
     </Grid>
   );
 };
--- src/ImageCard/ImageCard.jsx
+++ src/ImageCard/ImageCard.jsx
@@ -1,21 +1,29 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
-const ImageCard=()=> {
+const ImageCard = ({ locDetails }) => {
+  const [data, setData] = useState(null);
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+    <Card
+      className="border-0"
+      style={{ width: '18rem', height: '25rem !important' }}
+    >
+      <Card.Img src={locDetails.images[0]} className="img-fluid" />
+      <Card.Body className="p-0">
+        <Card.Title>{locDetails.address}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          172 kms away
+          <br />
+          10-15 september
+          <br />
+          <span className="fw-bold">₹5600</span> night
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
   );
-}
+};
 
 export default ImageCard;
